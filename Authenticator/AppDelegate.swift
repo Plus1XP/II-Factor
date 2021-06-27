2e0b1d622c2fae61dee55b048dab7d8c7ae0a9ad
Plus1XP <evldev@protonmail.ch>
2021-06-27 19:26:42 +0100

    Refactor(CloudKit): Change Persistant container to include cloudkit

--- Authenticator/AppDelegate.swift
+++ Authenticator/AppDelegate.swift
@@ -25,14 +25,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         // MARK: - Core Data stack
 
-        lazy var persistentContainer: NSPersistentContainer = {
+        lazy var persistentContainer: NSPersistentCloudKitContainer = {
                 /*
                  The persistent container for the application. This implementation
                  creates and returns a container, having loaded the store for the
                  application to it. This property is optional since there are legitimate
                  error conditions that could cause the creation of the store to fail.
                  */
-                let container = NSPersistentContainer(name: "Authenticator")
+                let container = NSPersistentCloudKitContainer(name: "Authenticator")
                 container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                         if let error = error as NSError? {
                                 /*
@@ -46,6 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                 logger.debug("Unresolved error \(error), \(error.userInfo)")
                         }
                 })
+                container.viewContext.automaticallyMergesChangesFromParent = true
                 return container
         }()
 
