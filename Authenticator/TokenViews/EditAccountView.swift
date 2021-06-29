e411e1b639c4fabbd190245150113436ddbd137e
Plus1XP <evldev@protonmail.ch>
2021-06-29 22:46:25 +0100

    Revert "Fix TextField in macCatalyst"
    This reverts commit 913ecb2e71a08df090abf2ecc0f9f5652653fcb5.

--- Authenticator/TokenViews/EditAccountView.swift
+++ Authenticator/TokenViews/EditAccountView.swift
@@ -1,110 +1,101 @@
 import SwiftUI
 
 struct EditAccountView: View {
-    
-    @Binding var isPresented: Bool
-    let token: Token
-    let tokenIndex: Int
-    let completion: (Int, String, String, String) -> Void
-    
-    @State private var displayIssuer: String = ""
-    @State private var displayAccountName: String = ""
-    @State private var displayGroup: String = TokenGroupType.None.rawValue
-    
-    var body: some View {
-        NavigationView {
-            ZStack {
-                GlobalBackgroundColor().ignoresSafeArea()
-                ScrollView {
-                    VStack {
-                        HStack {
-                            Text("Issuer").font(.headline)
-                            Spacer()
+
+        @Binding var isPresented: Bool
+        let token: Token
+        let tokenIndex: Int
+        let completion: (Int, String, String, String) -> Void
+
+        @State private var displayIssuer: String = ""
+        @State private var displayAccountName: String = ""
+        @State private var displayGroup: String = TokenGroupType.None.rawValue
+
+        var body: some View {
+                NavigationView {
+                        ZStack {
+                                GlobalBackgroundColor().ignoresSafeArea()
+                                ScrollView {
+                                        VStack {
+                                                HStack {
+                                                        Text("Issuer").font(.headline)
+                                                        Spacer()
+                                                }
+                                                #if targetEnvironment(macCatalyst)
+                                                TextField(token.displayIssuer, text: $displayIssuer)
+                                                        .disableAutocorrection(true)
+                                                        .autocapitalization(.words)
+                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                                #else
+                                                TextField(token.displayIssuer, text: $displayIssuer)
+                                                        .disableAutocorrection(true)
+                                                        .autocapitalization(.words)
+                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                                #endif
+                                        }.padding()
+                                        
+                                        VStack {
+                                                HStack {
+                                                        Text("Account Name").font(.headline)
+                                                        Spacer()
+                                                }
+                                                #if targetEnvironment(macCatalyst)
+                                                TextField(token.displayAccountName, text: $displayAccountName)
+                                                        .keyboardType(.emailAddress)
+                                                        .disableAutocorrection(true)
+                                                        .autocapitalization(.none)
+                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                                #else
+                                                TextField(token.displayAccountName, text: $displayAccountName)
+                                                        .keyboardType(.emailAddress)
+                                                        .disableAutocorrection(true)
+                                                        .autocapitalization(.none)
+                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                                #endif
+                                        }.padding(.horizontal)
+                                        
+                                        VStack {
+                                            HStack {
+                                                Text("Group").font(.headline)
+                                                Spacer()
+                                            }
+                                            TokenGroupAccountButtonStyleView(buttonSelected: $displayGroup)
+                                        }.padding()
+                                    
+                                        HStack {
+                                                // TODO: Localization
+                                                Text("NOTE: Changes would not apply to the Key URI")
+                                                        .font(.footnote)
+                                                        .foregroundColor(Color.secondary)
+                                                Spacer()
+                                        }.padding()
+                                }
                         }
-                        #if targetEnvironment(macCatalyst)
-                        TextField(token.displayIssuer, text: $displayIssuer)
-                            .disableAutocorrection(true)
-                            .autocapitalization(.words)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        #else
-                        EnhancedTextField(placeholder: token.displayIssuer,
-                                          text: $displayIssuer,
-                                          autocorrection: .no,
-                                          autocapitalization: .words)
-                            .padding(8)
-                            .fillBackground(cornerRadius: 8)
-                        #endif
-                    }
-                    .padding()
-                    
-                    VStack {
-                        HStack {
-                            Text("Account Name").font(.headline)
-                            Spacer()
+                        .navigationTitle("title.edit_account")
+                        .toolbar {
+                                ToolbarItem(placement: .navigationBarLeading) {
+                                        Button(action: {
+                                                isPresented = false
+                                        }) {
+                                                Text("Cancel")
+                                        }
+                                }
+                                ToolbarItem(placement: .navigationBarTrailing) {
+                                        Button(action: {
+                                                displayIssuer = displayIssuer.trimming()
+                                                displayAccountName = displayAccountName.trimming()
+                                                displayGroup = displayGroup.trimming()
+                                                completion(tokenIndex, displayIssuer, displayAccountName, displayGroup)
+                                                isPresented = false
+                                        }) {
+                                                Text("Done")
+                                        }
+                                }
                         }
-                        #if targetEnvironment(macCatalyst)
-                        TextField(token.displayAccountName, text: $displayAccountName)
-                            .keyboardType(.emailAddress)
-                            .disableAutocorrection(true)
-                            .autocapitalization(.none)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        #else
-                        EnhancedTextField(placeholder: token.displayAccountName,
-                                          text: $displayAccountName,
-                                          keyboardType: .emailAddress,
-                                          autocorrection: .no,
-                                          autocapitalization: UITextAutocapitalizationType.none)
-                            .padding(8)
-                            .fillBackground(cornerRadius: 8)
-                        #endif
-                    }
-                    .padding(.horizontal)
-                    
-                    VStack {
-                        HStack {
-                            Text("Group").font(.headline)
-                            Spacer()
-                        }
-                        TokenGroupAccountButtonStyleView(buttonSelected: $displayGroup)
-                    }
-                    .padding()
-                    
-                    HStack {
-                        // TODO: Localization
-                        Text("NOTE: Changes would not apply to the Key URI")
-                            .font(.footnote)
-                            .foregroundColor(Color.secondary)
-                        Spacer()
-                    }
-                    .padding()
+                }.onAppear {
+                        displayIssuer = token.displayIssuer
+                        displayAccountName = token.displayAccountName
+                        displayGroup = token.displayGroup
                 }
-            }
-            .navigationTitle("title.edit_account")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        isPresented = false
-                    }) {
-                        Text("Cancel")
-                    }
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        displayIssuer = displayIssuer.trimming()
-                        displayAccountName = displayAccountName.trimming()
-                        displayGroup = displayGroup.trimming()
-                        completion(tokenIndex, displayIssuer, displayAccountName, displayGroup)
-                        isPresented = false
-                    }) {
-                        Text("Done")
-                    }
-                }
-            }
         }
-        .onAppear {
-            displayIssuer = token.displayIssuer
-            displayAccountName = token.displayAccountName
-            displayGroup = token.displayGroup
-        }
-    }
 }
