4f2298755adf1f32c8ae9279bbd81bd205de7113
Bing Zheung <bing@ososo.io>
2021-12-11 17:34:15 +0800

    Refresh views after generateCodes()
    # Conflicts:
    #	Authenticator/ContentView.swift

--- Authenticator/MainView.swift
+++ Authenticator/MainView.swift
@@ -23,6 +23,7 @@ struct MainView: View {
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State private var timeRemaining: Int = 30 - (Int(Date().timeIntervalSince1970) % 30)
     @State private var codes: [String] = Array(repeating: "000000", count: 50)
+    @State private var isRefreshed: Bool = false
     @State private var isSheetPresented: Bool = false
     @State private var isFileImporterPresented: Bool = false
     @State private var editMode: EditMode = .inactive
@@ -83,6 +84,7 @@ struct MainView: View {
             .onReceive(NotificationCenter.default.publisher(for: Notification.Name(rawValue: "AppLocked"))) {
                 _ in self.isSheetPresented = false; self.isFileImporterPresented = false
             }
+            .animation(.default, value: isRefreshed)
             .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
                 generateCodes()
                 clearTemporaryDirectory()
@@ -442,6 +444,7 @@ struct MainView: View {
         }
         let generated: [String] = fetchedTokens.map { code(of: $0) }
         codes = generated + placeholder
+        isRefreshed.toggle()
     }
     
     private func code(of tokenData: TokenData) -> String {
