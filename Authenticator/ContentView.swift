65c4de2fd87586c2dbd8db1145a60da3b35f56b5
Plus1XP <evldev@protonmail.ch>
2022-01-13 12:17:40 +0000

    Feat: Add banner (Disabled) to confirm biometric status

--- Authenticator/ContentView.swift
+++ Authenticator/ContentView.swift
@@ -1,6 +1,13 @@
 import SwiftUI
 import CoreData
 
+enum AuthenticationState {
+    case waiting
+    case error
+    case failure
+    case successful
+}
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) var context
     @EnvironmentObject var settings: SettingsStore
@@ -9,6 +16,33 @@ struct ContentView: View {
     @State private var isLockEnabled = true
     @State private var isAutoLockEnable = true
     @State private var defaultGroup = TokenGroupType.None.rawValue
+    
+    @State private var canShowAuthenticatedBanner = false
+    @State private var authenticationState = AuthenticationState.waiting
+    
+    private let authenticationWaiting: BannerData = BannerData(
+        title: "Authentication Waiting",
+        level: .info,
+        style: .popUp
+    )
+    
+    private let authenticationError: BannerData = BannerData(
+        title: "Authentication Error, Incorrect Biometrics or Password, User Cancelled",
+        level: .warning,
+        style: .popUp
+    )
+    
+    private let authenticationFailure: BannerData = BannerData(
+        title: "Authentication Failure, No Biometrics or Password Set",
+        level: .error,
+        style: .popUp
+    )
+    
+    private let authenticationSuccessful: BannerData = BannerData(
+        title: "Authentication Successful",
+        level: .success,
+        style: .popUp
+    )
     
     var body: some View {
         ZStack {
@@ -52,6 +86,9 @@ struct ContentView: View {
             debugPrint("Moving to the foreground!")
             ValidateBiometrics()
         }
+//        .banner(isPresented: $canShowAuthenticatedBanner, data: GetAuthenticationBannerData(), action: {
+//            // Place action here..
+//        })
 //        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { (_) in
 //            debugPrint("Moving to the background!")
 //            isBackground = true
@@ -88,20 +125,47 @@ struct ContentView: View {
     }
     
     private func ValidateBiometrics() -> Void {
+        authenticationState = .waiting
         let biometric = BiometricService()
         biometric.canEvaluate { (canEvaluate, _, canEvaluateError) in
             guard canEvaluate else {
                 debugPrint(canEvaluateError?.localizedDescription ?? "Authentication Failure, No Biometrics or Password Set")
+                authenticationState = .failure
                 return
             }
             biometric.evaluate { (success, error) in
                 guard success else {
                     debugPrint(error?.localizedDescription ?? "Authentication Error, Incorrect Biometrics or Password, User Cancelled")
+                    authenticationState = .error
                     return
                 }
                 debugPrint("Authentication Successful")
+                authenticationState = .successful
                 self.isAppLocked = false
             }
+        }
+        canShowAuthenticatedBanner = true
+        DismissBanner()
+    }
+    
+    private func GetAuthenticationBannerData() -> BannerData {
+        switch authenticationState {
+        case .waiting:
+            return authenticationWaiting
+        case .error:
+            return authenticationError
+        case .failure:
+            return authenticationFailure
+        case .successful:
+            return authenticationSuccessful
+        }
+    }
+    
+    private func DismissBanner() {
+        // Delay of 7 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
+            canShowAuthenticatedBanner = false
+            debugPrint("Banner Dismissed")
         }
     }
 }
